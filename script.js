4c7d768ae1e3193a2406eee9070daebdfae7304d
Alexis Guzman <alexisguzman157@outlook.com>
2021-08-31 21:49:09 -0400

    added and fixed my fetch, fixed typos that prevented my java to working. Added Temperature humidity and windspeed when searching for a city weather

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var cityFormEl = document.querySelector('#city-form');
-var cityButtonsEl = document.querySelector('#city-buttons');
+//var cityButtonsEl = document.querySelector('#city-buttons');
 var cityInputEl = document.querySelector('#cityname');
 var weatherContainerEl = document.querySelector('#city-container');
 var citySearchName = document.querySelector('#city-search-name');
 var APIKey = '8e571819bbb8c12a8df6f271868e5021';
 var city;
+
+var windInputEl =document.querySelector('#wind');
+var tempInputEL =document.querySelector('#temp');
+var humidityInputEl = document.querySelector('#humid');
 
 var SubmitHandler = function (event) { // should be able to enter the city name and return the weather for the stated city
     event.preventDefault();
@@ -23,7 +27,7 @@ var SubmitHandler = function (event) { // should be able to enter the city name 
 };
 
 
-var button
+
 
 var getCityWeather = function (city) {
     var WeatherApi = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
@@ -44,30 +48,45 @@ var getCityWeather = function (city) {
     });
 };
 
-var displayWeather = function (city, searchName) {
-    if (city.length === 0) {
+var displayWeather = function (cityData, cityName1) {
+    if (cityData.length === 0) {
         weatherContainerEl.textContent = 'No city  found';
         return;
-    }
+ 
+    }     
+    console.log(cityData)
+    citySearchName.textContent = cityName1
+windInputEl.textContent = 'Wind Speed: ' + cityData.wind.speed +" MPH";
+tempInputEL.textContent = 'Temperature: ' + cityData.main.temp + " Kelvin";
+humidityInputEl.textContent = 'Humidity: ' + cityData.main.humidity + "%";
 
-    citySearchName.textContent = searchName 
 
-    for (var i = 0; i < city.length; i++) {
-        var cityData = city[i].location.cityinput + '/' + [i].name
-    }
-    var cityEl = document.createElement('h2');
-    cityEl.classList = 'list-item flex-row justify-space-between align-center';
+//humidity, temperature, UV index - 
+//uv index- color change 
+// five day forcast
+//search history
 
-    var infoEl = document.createElement('span');
-    infoEl.textContent = cityData;
 
-    //cityEl.appendChild(infoEl);
 
-   // var currentWetEl = document.createElement('span');
-    currentWetEl.classList = 'flex-row align-center';
 
-   //cityEl.appendChild(currentWetEl);
-   //weatherContainerEl.appendChild(cityEl);
+    // for (var i = 0; i < city.length; i++) {
+    //     var cityData = city[i].location.cityinput + '/' + [i].name
+    // }
+    
+//     var cityEl = document.createElement('h2');
+//     cityEl.classList = 'list-item flex-row justify-space-between align-center';
+
+//     var infoEl = document.createElement('span');
+//     infoEl.textContent = cityData;
+
+//     cityEl.appendChild(infoEl);
+
+//     var currentWetEl = document.createElement('span');
+//     currentWetEl.classList = 'flex-row align-center';
+
+//    cityEl.appendChild(currentWetEl);
+//    weatherContainerEl.appendChild(cityEl);
+// 
 }
 
 
@@ -75,8 +94,8 @@ var displayWeather = function (city, searchName) {
 
 
 
+//city.addEventListener()
 
 
-
-cityFormEl.addEventListner('submit', SubmitHandler);
-cityButtonsEl.addEventListener('click', buttonClickHandler);
+cityFormEl.addEventListener('submit', SubmitHandler);
+//cityButtonsEl.addEventListener('click', buttonClickHandler);
